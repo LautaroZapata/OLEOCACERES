973d059781a4ed3f83fd29de8f50fca00a2a6ba7
LautaroZapata <oratualzc@gmail.com>
2022-02-04 14:39:59 -0300

    page finished

---
+++ js/index.js
@@ -0,0 +1,30 @@
+window.sr = ScrollReveal();
+
+
+sr.reveal('nav', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-50px'
+
+});
+
+sr.reveal ('.sectionEmpresa', {
+    duration: 5000,
+    origin: 'rigth',
+    distance: '-300px'
+});
+
+sr.reveal ('.sectionTrabajos',{
+    duration: 5000,
+    origin: 'top',
+    distance: '-400px'
+})
+
+
+
+sr.reveal ('.sectionNuestrosClientes',{
+    duration: 5000,
+    origin: 'left',
+    distance: '-100px'
+})
+
